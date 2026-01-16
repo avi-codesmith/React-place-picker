db8ab623d9bd01d92331da875e3200be0f05430d
avi-codesmith <avi.codesmith@gmail.com>
2026-01-16 17:59:31 +0530

    useEffect's cleanup function

--- src/components/DeleteConfirmation.js
+++ src/components/DeleteConfirmation.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [num, setNum] = useState(3);
+  console.log("set timer");
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setNum((prev) => {
+        if (prev === 1) {
+          clearInterval(countdown);
+          onConfirm();
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(countdown);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -8,7 +30,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           No
         </button>
         <button onClick={onConfirm} className="button">
-          Yes
+          Yes{` ( after ${num} seconds )`}
         </button>
       </div>
     </div>
